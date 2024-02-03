d4e778b1548f7bc9f8565293bbae66448aad658b
0xZurvan <75655454+0xZurvan@users.noreply.github.com>
2024-02-03 15:13:17 -0400

    chore: dependencies updates

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   components: [
     {
       path: './components',
