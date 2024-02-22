524b158ddc9f051253e9061eba5ec947718f8d6a
0xZurvan <75655454+0xZurvan@users.noreply.github.com>
2024-02-21 21:34:29 -0400

    refactor: adding client-only where only is needed

--- client/components/layout/DesktopNav.vue
+++ client/components/layout/DesktopNav.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-row items-center justify-between">
+    <div class="flex flex-row space-x-4">
+      <!-- Logo -->
+      <div class="w-32 h-10 rounded-lg bg-neutral-950"></div>
+      <SearchBar />
+    </div>
     <ClientOnly>
-      <div class="flex flex-row space-x-4">
-        <!-- Logo -->
-        <div class="w-32 h-10 rounded-lg bg-neutral-950"></div>
-        <SearchBar />
-      </div>
       <NuxtLink v-if="user.name !== '' && user.id !== 0" to="/profile">
         <UserTab :name="user.name" :image="(user.image as string)" />
       </NuxtLink>
-      
+    
       <div v-else class="flex flex-row items-center gap-4">
         <NuxtLink to="/sign-in">
           <Button variant="outline" class="w-full text-white border-none bg-neutral-950">Sign in</Button>
